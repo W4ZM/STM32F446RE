486b7b62340f8bc5e8c9070cd401b016211a7e09
W4ZM <dzwassim95@gmail.com>
2025-10-08 18:47:26 +0100

    init

--- src/main.c
+++ src/main.c
@@ -1,25 +1,63 @@
 #include <stdint.h>
+#include <string.h>
 #include "stm32f4xx.h"
 
-#define LED_PIN 5
+void usart_init(void);
+void dma_it_init();
+void dma_it_start(uint32_t src_addr, uint32_t src_len);
+void dma1_stream6_handler();
+
+char msg[] = "This message is from DMA !\r\n";
 
 void main(void){
   
-  // enable GPIOA clock.
-  RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOAEN_Pos);
+  usart_init();
 
-  // ensure the clock is really stable before accessing the peripheral registers.
-  volatile uint32_t dummy;
-  dummy = RCC->AHB1ENR;
-  dummy = RCC->AHB1ENR;
+  dma_it_init();
+  dma_it_start((uint32_t)msg, strlen(msg));
 
-  // configure PA5 as output.
-  GPIOA->MODER |= (1 << GPIO_MODER_MODER5_Pos)
+  // a small delay to be able to see the UART message.
+  for (uint32_t i = 0; i < 500000; i++);
 
-  // toggle PA5 in an infinite loop with a delay.
-  while(1)
+  //Enable UART in DMA mode
+  SET_BIT(USART2->CR3, USART_CR3_DMAT);
+
+  while (1);
+}
+
+void dma1_stream6_handler(){
+
+  //Disable UART in DMA mode
+  CLEAR_BIT(USART2->CR3, USART_CR3_DMAT);
+
+  // transfer error interrupt.
+  if (READ_BIT(DMA1->HISR, DMA_HISR_TEIF6) != RESET)
   {
-    GPIOA->ODR ^= (1 << LED_PIN);    
-    for (uint32_t i = 0; i < 1000000; i++);
+    // disable the interrupt.
+    CLEAR_BIT(DMA1_Stream6->CR, DMA_SxCR_TEIE);
+
+    // clear the interrupt flag.
+    SET_BIT(DMA1->HIFCR, DMA_HIFCR_CTEIF6);
+
+    while(1);
+  }
+
+  // direct mode error interrupt.
+  if (READ_BIT(DMA1->HISR, DMA_HISR_DMEIF6) != RESET)
+  {
+    // clear the interrupt flag.
+    SET_BIT(DMA1->HIFCR, DMA_HIFCR_CDMEIF6);
+    while(1);
+  }
+  
+  // transfer complete interrupt.
+  if (READ_BIT(DMA1->HISR, DMA_HISR_TCIF6) != RESET)
+  { 
+    // disable the interrupt.
+    CLEAR_BIT(DMA1_Stream6->CR, DMA_SxCR_TCIE);
+    
+    // clear the interrupt flag.
+    SET_BIT(DMA1->HIFCR, DMA_HIFCR_CTCIF6);
+    return;
   }
 }
